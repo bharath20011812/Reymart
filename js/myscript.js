e9f3b41e2d50fd202047295143dbe7b934e1f1fd
Bharathkumar <85354718+bharath20011812@users.noreply.github.com>
2024-11-05 15:33:03 +0530

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -221,3 +221,43 @@ function animateCounter(element, target) {
 
   updateCounter();
 }
+
+// faq
+$(document).ready(function () {
+  $(".faqs-container .faq-singular:first-child").addClass("active").children(".faq-answer").slideDown();//Remove this line if you dont want the first item to be opened automatically.
+  $(".faq-question").on("click", function () {
+    if ($(this).parent().hasClass("active")) {
+      $(this).next().slideUp();
+      $(this).parent().removeClass("active");
+    }
+    else {
+      $(".faq-answer").slideUp();
+      $(".faq-singular").removeClass("active");
+      $(this).parent().addClass("active");
+      $(this).next().slideDown();
+    }
+  });
+});
+
+
+// pagination
+
+// jQuery Plugin: http://flaviusmatis.github.io/simplePagination.js/
+
+var items = $(".faqs-container .faq-singular");
+var numItems = items.length;
+var perPage = 4;
+
+items.slice(perPage).hide();
+
+$('#pagination-container').pagination({
+  items: numItems,
+  itemsOnPage: perPage,
+  prevText: "&laquo;",
+  nextText: "&raquo;",
+  onPageClick: function (pageNumber) {
+    var showFrom = perPage * (pageNumber - 1);
+    var showTo = showFrom + perPage;
+    items.hide().slice(showFrom, showTo).show();
+  }
+});
